f88dec4149e77cf6e08219578a343fb72d55a959
Lexi Rose <north@tinymagnet.com>
2023-10-15 16:13:19 -0500

    Update AssemblyInfo.cs - Quest platform support
    Updated assembly defs so the game is recognized on Quest.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -35,4 +35,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: MelonInfo(typeof(Trashbin.Trashbin), "Trashbin", "2.0.0", "Moinn")]
+#if QUEST
+[assembly: MelonGame("kluge", "SynthRiders")]
+#else
 [assembly: MelonGame("Kluge Interactive", "SynthRiders")]
+#endif
